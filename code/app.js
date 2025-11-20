5f7bee8222db76b906b54323a564623e19dde795
unknown <quynh@example.com>
2025-11-20 09:15:14 +0700

    Update line 5 on main branch

---
+++ code/app.js
@@ -0,0 +1,6 @@
+// app.js
+console.log("This is the beginning of the app");
+console.log("This is line 2");
+console.log("This is line 3");
+console.log("This is line 4");
+console.log("Hello from main"); // Dòng 5 đã được sửa
