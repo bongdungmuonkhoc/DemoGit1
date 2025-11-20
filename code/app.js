ef6bec875c65c53b7f41c855d016b2cddc2f36cc
unknown <quynh@example.com>
2025-11-20 09:16:15 +0700

    Add home feature, changed line 5

--- code/app.js
+++ code/app.js
@@ -3,4 +3,4 @@ console.log("This is the beginning of the app");
 console.log("This is line 2");
 console.log("This is line 3");
 console.log("This is line 4");
-console.log("Hello from main"); // Dòng 5 đã được sửa
+console.log("Hello from feature/home"); // Dòng 5 đã được sửa
